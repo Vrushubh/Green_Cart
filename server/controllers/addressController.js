72162f148bd7e54dbb5aa856b2b801d4bbf04ae9
Vrushubh <vrushubhgandhi@gmail.com>
2025-06-16 00:57:10 +0530

    First Commit

---
+++ server/controllers/addressController.js
@@ -0,0 +1,34 @@
+
+//Add Address : api/address/add
+
+import Address from "../models/Address.js";
+
+export const addAddress = async(req,res)=>{
+    try {
+        const {address } = req.body;
+        const userId = req.userId;
+
+        console.log("req.body:", req.body);
+        console.log("req.userId:", req.userId);
+
+        await Address.create({...address , userId});
+        res.json({success : true , message : "Address Added"})
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+    }
+}
+
+//Get Address : api/address/get
+
+export const getAddress = async(req,res)=>{
+    try {
+        const userId = req.userId;
+        const addresses = await Address.find({userId});
+        res.json({success : true ,addresses})
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+    }
+}
+
---
+++ server/controllers/cartController.js
@@ -0,0 +1,15 @@
+// Update User CartData : /api/cart/update
+
+import User from "../models/user.js";
+
+export const updateCart = async(req,res)=>{
+    try {
+        const {  cartItems} = req.body;
+        const userId = req.userId;
+        await User.findByIdAndUpdate(userId,{cartItems});
+        res.json({success : true , message : "Cart Updated"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success : false , message : error.message})
+    }
+}
